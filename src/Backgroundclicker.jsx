ed7c1589b182669527940a9678b25dbdcf3dfe0d
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-11-05 12:04:41 +0530

    commit

--- src/Backgroundclicker.jsx
+++ src/Backgroundclicker.jsx
@@ -3,21 +3,33 @@ import "../src/index.css";
 
 const Backgroundclicker = () => {
   let purple = "#62E150";
+  let MainColor = "#2C3E50"
   const [bg, setBg] = useState(purple);
-  const [names ,setNames] = useState("click me")
+  const [names ,setNames] = useState("click me ❤️");
+  const [bgbtn , setbgbtn] = useState(MainColor)
   
 
 //   EVENT PR KAAM HORA HAI
   const bgChange = () => {
     let Newbg = "#50A5AC";
+    let Newbtncolor = "#626567"
     setBg(Newbg);
-    setNames('send me 🦈')
+    setNames('send me 🎏')
+    setbgbtn(Newbtncolor)
   };
+
+  const bgBack = () =>{
+    setBg(purple);
+    setNames(`let's back 🐺`)
+    setbgbtn(MainColor)
+  }
 
   return (
     <Fragment>
       <div style={{ backgroundColor: bg }}>
-        <button onDoubleClick={bgChange}>{names}</button>
+        <button onMouseEnter={bgChange} onMouseLeave={bgBack} style={{
+            backgroundColor : bgbtn
+        }}>{names}</button>
       </div>
     </Fragment>
   );
