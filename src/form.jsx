95779a4e79bb1f87360853b7e7c572339b7fb453
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-11-08 00:01:01 +0530

    form in react

--- src/form.jsx
+++ src/form.jsx
@@ -1,11 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 const Form = () => {
+const [name,setName] = useState();
+const [fullName ,setfullName] = useState()
+    
+const inputEvent =(event)=>{
+    setName(event.target.value)
+}
+const onShow =()=>{
+    setfullName(name);
+}
   return (
     <Fragment>
-      <h1>HELLO ..!</h1>
-      <input type="text" placeholder="Enter your Name "></input>
-      <button> CLICK ME </button>
+      <h1>HELLO ..!{fullName}</h1>
+      <input type="text" placeholder="Enter your Name" onChange={inputEvent}></input>
+      <button onClick={onShow}> CLICK ME </button>
     </Fragment>
   );
 };
