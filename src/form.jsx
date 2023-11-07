225590103fcbc9ee500cd3a8c58aa2b040b46d56
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-11-07 23:40:29 +0530

    form in react

---
+++ src/form.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const Form = () => {
+  return (
+    <Fragment>
+      <h1>HELLO ..!</h1>
+      <input type="text" placeholder="Enter your Name "></input>
+      <button> CLICK ME </button>
+    </Fragment>
+  );
+};
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 // import App from "./App";
 import Backgroundclicker from "./Backgroundclicker";
 import App from "./App";
+import Form from "./form";
 
 ReactDOM.render(
   <Fragment>
   <Backgroundclicker/>
   <App/>
+  <Form/>
   </Fragment>,document.getElementById("root")
 );
